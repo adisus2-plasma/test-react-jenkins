b6c00afbffcf8e1abffbf76737f0ee4df7a8dcb1
Pattarapol Sangsunee <pattarapol262@gmail.com>
2021-07-05 17:19:21 +0700

    [FIX]test6

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and Hello World
+          Edit <code>src/App.js</code> and Hello World shit
         </p>
         <a
           className="App-link"
